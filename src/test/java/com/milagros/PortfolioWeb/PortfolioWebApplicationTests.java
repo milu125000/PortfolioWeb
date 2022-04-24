0194ecfc091c5ceb46da3494203862b20fbe20d2
Milagros Sassenus <milu125000@gmail.com>
2022-04-24 18:23:09 -0300

    Building proyect

---
+++ src/test/java/com/milagros/PortfolioWeb/PortfolioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milagros.PortfolioWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
