762aa1e2832d876d8694a82ab15527c005da8b4b
Milagros Sassenus <milu125000@gmail.com>
2022-04-24 18:58:46 -0300

    boostrap html added to "/"

--- src/main/java/com/milagros/PortfolioWeb/controllers/HomeController.java
+++ src/main/java/com/milagros/PortfolioWeb/controllers/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @GetMapping("")
     public String index(){
-        return "home/index";
+        return "/index";
     }
     
 }
